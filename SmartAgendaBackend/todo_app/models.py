44b4cd965709f5883826cf948733037521d9caa7
MurielNdomo <ndomomuriel@gmail.com>
2021-05-21 16:18:19 +0100

    Models update

--- SmartAgendaBackend/todo_app/models.py
+++ SmartAgendaBackend/todo_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.models):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    send_notifications = models.BooleanField(default=False)
+    hide = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class TodoList(Item):
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name='sub_lists')
+
+class TodoItem(Item):
+    due_date = models.DateTimeField()
+    send_notifications = models.BooleanField(default=False)
+    # https://stackoverflow.com/questions/38388423/what-does-on-delete-do-on-django-models
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE, related_name='items')
+
+
--- SmartAgendaBackend/wallet_app/models.py
+++ SmartAgendaBackend/wallet_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+EXPENSE = 'EXP'
+INCOME = 'INC'
+
+# TODO: add internalization
+TRANSACTION_AVAILABLE_TYPES = [
+    (EXPENSE, 'Dépense')
+    (INCOME, 'Revenu')
+]
+# examples fo rubric values: factures, alimentation, dimes, offrandes, salaire, habillement, coiffure, don 
+class TransactionRubric(models.Model):
+    name = models.CharField(max_length=250)
+
+class Transaction(models.Models):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    amount = models.money 
+    # TODO: what does auto_now_add means ?, the other options
+    created_date = models.DateTimeField(auto_now_add=True)
+    rubric = models.ForeignKey(TransactionRubric, on_delete=models.SET_NULL, related_name='transactions')
+    transaction_type = models.CharField(
+        max_length=3,
+        choices=TRANSACTION_AVAILABLE_TYPES,
+        default=EXPENSE,
+    )
+
